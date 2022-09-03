55fb8c91d82cf12dee4bcbfbc716279d775e17cd
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-09-03 10:34:02 +0900

    算出プロパティとメソッドの違い

--- js/template.js
+++ js/template.js
@@ -2,7 +2,25 @@ Vue.createApp({
   data: function() {
     return {
       message: 'みなさんこんにちは',
-      url: 'https://media.connec10.co.jp/'
+      url: 'https://media.connec10.co.jp/',
+      email: 'Y-Suzuki@example.com',
+      current: new Date().toLocaleString(),
     };
+  },
+  computed: {
+    localEmail: function () {
+      return this.email.split('@')[0].toLowerCase();
+    },
+    random: function () {
+      return Math.random();
+    }
+  },
+  methods: {
+    onclick: function () {
+      this.current = new Date().toLocaleString();
+    },
+    randomm: function () {
+      return Math.random();
+    }
   }
 }).mount('#app');
