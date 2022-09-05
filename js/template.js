6bca023c794270b733a79d06ddce0a76204dbfd4
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-09-06 08:33:39 +0900

    リアクティブデータの学習

--- js/template.js
+++ js/template.js
@@ -1,14 +1,23 @@
 Vue.createApp({
-  data: function() {
+  data() {
     return {
       message: 'みなさんこんにちは',
       url: 'https://media.connec10.co.jp/',
       email: 'Y-Suzuki@example.com',
       current: new Date().toLocaleString(),
+      cur: new Date().toLocaleString(),
     };
   },
+  renderTracked(e) {
+    console.log('renderTracked')
+    console.log(e)
+  },
+  renderTriggered(e) {
+    console.log('renderTri')
+    console.log(e)
+  },
   computed: {
-    localEmail: function () {
+    localEmail() {
       return this.email.split('@')[0].toLowerCase();
     },
     random: function () {
@@ -16,11 +25,19 @@ Vue.createApp({
     }
   },
   methods: {
-    onclick: function () {
+    onclick() {
       this.current = new Date().toLocaleString();
     },
     randomm: function () {
       return Math.random();
     }
+  },
+  created() {
+    this.timer = setInterval(() => {
+      this.cur = new Date()
+    }, 1000)
+  },
+  beforeUnmount() {
+    clearInterval(this.timer)
   }
 }).mount('#app');
