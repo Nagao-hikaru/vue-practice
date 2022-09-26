9c85f41144137ff9fc4b0a6bda05a952b6bb82c8
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-09-27 08:11:57 +0900

    コミット

--- js/component.js
+++ js/component.js
@@ -1,9 +1,26 @@
-Vue.createApp({})
-.component('my-hello', {
-  template: '<div>こんにちは、{{ name }}!</div>',
-  data() {
-    return {
-      name: 'Vue.js'
-    };
+// Vue.createApp({})
+// .component('my-hello', {
+//   template: '<div>こんにちは、{{ name }}!</div>',
+//   data() {
+//     return {
+//       name: 'Vue.js'
+//     };
+//   }
+// }).mount('#app');
+
+const Mychild = {
+  template: '<div>今日へ、Comp2</div>'
+};
+const Myparent = {
+  components: {
+    'my-child': Mychild,
+  },
+  template: '<div>今日へ</div><my-child></my-child>'
+};
+
+Vue.createApp({
+  components: {
+    'my-parent': Myparent,
+    'my-child': Mychild,
   }
-}).mount('#app');
+}).mount('#app')
