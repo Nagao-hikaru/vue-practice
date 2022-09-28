939219a0dd65ebc7ee4b992e088713d015553046
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-09-29 08:45:46 +0900

    $emitの学習

--- js/component.js
+++ js/component.js
@@ -1,26 +1,16 @@
-// Vue.createApp({})
-// .component('my-hello', {
-//   template: '<div>こんにちは、{{ name }}!</div>',
-//   data() {
-//     return {
-//       name: 'Vue.js'
-//     };
-//   }
-// }).mount('#app');
-
-const Mychild = {
-  template: '<div>今日へ、Comp2</div>'
-};
-const Myparent = {
-  components: {
-    'my-child': Mychild,
-  },
-  template: '<div>今日へ</div><my-child></my-child>'
-};
-
-Vue.createApp({
-  components: {
-    'my-parent': Myparent,
-    'my-child': Mychild,
-  }
-}).mount('#app')
+Vue.createApp({})
+  .component('my-counter', {
+    props: ['step'],
+    emits: ['plusMinus'],
+    template: '<button type="button" @click="onclick">{{ step }}</button>',
+    data() {
+      return {
+        current: 0
+      };
+    },
+    methods: {
+      onclick() {
+        this.$emit('plusMinus', Number(this.step));
+      }
+    }
+  }).mount('#app')
