45130c119fcf673d358380f322bae2f6dbf36565
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-10-11 17:41:50 +0900

    $emitの学習

--- js/component.js
+++ js/component.js
@@ -1,13 +1,19 @@
-Vue.createApp({})
+Vue.createApp({
+  data() {
+    return {
+      current: 0
+    }
+  },
+  methods: {
+    onplus(e) {
+      this.current += e;
+    }
+  }
+})
   .component('my-counter', {
     props: ['step'],
     emits: ['plusMinus'],
     template: '<button type="button" @click="onclick">{{ step }}</button>',
-    data() {
-      return {
-        current: 0
-      };
-    },
     methods: {
       onclick() {
         this.$emit('plusMinus', Number(this.step));
