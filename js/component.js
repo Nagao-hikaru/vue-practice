62373d5baa49eb94b5c996911f8c6911b5b343c3
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-10-26 08:42:16 +0900

    a

--- js/component.js
+++ js/component.js
@@ -1,18 +1,30 @@
 Vue.createApp({
+  created() {
+    this.interval = setInterval(() => {
+      this.current = (this.current + 1) % this.components.length;
+    }, 3000);
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  computed: {
+    currentBanner() {
+      return 'banner-' + this.components[this.current];
+    }
+  },
+  data() {
+    return {
+      current: 0,
+      components: ['member', 'new', 'env']
+    }
+  }
 })
-  .component('my-parent', {
-    provide: {
-      title: 'Vue 3実践入門'
-    },
-    template: '<div id="parent"><my-my /></div>'
+  .component('banner-member', {
+    template: '<h3>メンバー募集</h3>'
   })
-  .component('my-hello', {
-    template: '<div>こんにていは<slot>ゲスト</slot>さん！</div>'
+  .component('banner-new', {
+    template: '<h3>新メンバー募集</h3>'
   })
-  .component('my-my', {
-    template: '<div id="my"><my-child /></div>'
-  })
-  .component('my-child', {
-    inject: ['title'],
-    template: '<div id="child">{{ title }}</div>'
+  .component('banner-env', {
+    template: '<h3>いい環境</h3>'
   }).mount('#app')
